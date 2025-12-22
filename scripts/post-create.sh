93e60005f482cfb19163ac9ae35d7972a3a2234e
Yuxiang-Huang <yuxiangh142857@gmail.com>
2025-12-22 14:45:31 -0500

    chore: add devcontainer

---
+++ scripts/post-create.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env zsh
+
+# This script is run after the dev container is created.
+
+# Activate Bun completions in zsh on startup
+if ! grep -q 'source <(SHELL=zsh bun completions)' ~/.zshrc; then
+    echo 'source <(SHELL=zsh bun completions)' >>~/.zshrc
+fi
